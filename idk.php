fd70f16c508ee0a5e5a91b446473c636ff8ac09d
anishcse312 <anishbancse312@gmail.com>
2025-10-05 20:26:22 -0400

    hhh

--- idk.php
+++ idk.php
@@ -3,9 +3,10 @@
 echo "Enter a number: ";
 $n = intval(trim(fgets(STDIN))); // Read input from user
 
-// Print numbers from 1 to n
-for ($i = 1; $i <= $n; $i++) {
-    echo $i . PHP_EOL; // prints each number on a new line
-echo("Hi");
+if ($n >10){
+    echo ">10";
 }
+
+echo("Hi");
+
 ?>
